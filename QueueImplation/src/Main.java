e550365981b2f9ac13a3aeb08c2700d2d689f0b6
Raiyan <Raiyan-sharif@users.noreply.github.com>
2020-03-15 12:45:03 +0600

    Add files via upload

--- QueueImplation/src/Main.java
+++ QueueImplation/src/Main.java
@@ -1,4 +1,6 @@
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random; 
 public class Main {
 	public static void main (String[] args)
@@ -9,10 +11,21 @@ public class Main {
 		// create a queue of capacity 5
 		Queue q = new Queue(5);
 		
-		// This will take random number between 1 to 5 
+		// This will take random number between 1 to 5
+		//I First passenger arrives 
+		q.enqueue(rand.nextInt(5)+1);
+		
+		//III a Next passenger
+		
 		q.enqueue(rand.nextInt(5)+1);
 		q.enqueue(rand.nextInt(5)+1);
-		q.enqueue(rand.nextInt(5)+1);
+		
+		
+//		Calendar cal = Calendar.getInstance(); // creates calendar
+//	    cal.setTime(new Date()); // sets calendar time/date
+//	    cal.add(Calendar.HOUR_OF_DAY, 1); // adds one hour
+//	    cal.getTime(); // returns new date object, one hour in the future
+		
 		
 		System.out.println("Front element is: " + q.peek());
 		q.dequeue();
